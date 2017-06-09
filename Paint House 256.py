86b316142cd09d3fe1f5e821858a2ce3ee09dcc1
maston14 <maston14@hotmail.com>
2017-06-08 17:12:25 -0700

    Initial commit

---
+++ Paint House 256.py
@@ -0,0 +1,17 @@
+# Origin
+class Solution(object):
+    def minCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        if costs == []:
+            return 0
+
+        for i in range(1, len(costs)):
+            for j in range(len(costs[i])):
+                opt1 = costs[i - 1][(j + 1) % 3]
+                opt2 = costs[i - 1][(j + 2) % 3]
+                costs[i][j] += min(opt1, opt2)
+
+        return min(costs[len(costs) - 1])
